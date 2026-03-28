1799d988a82f8fa373fb9612d3d07158bc13750d
Fray117 <fray117@exploiter.id>
2026-03-29 03:30:24 +0700

    feat(Gallery.tsx): add new Gallery component with images and links for home page

---
+++ src/components/theme-blocks/home/Gallery.tsx
@@ -0,0 +1,53 @@
+import { Link } from "@/i18n/nav"
+import { useTranslations } from "next-intl"
+import Image from "next/image"
+
+export default function Component() {
+	const t = useTranslations('HomePage.gallery')
+
+	return (
+		<div className="bg-accent px-4 py-32 min-w-screen">
+			<div className="flex flex-col sm:flex-row justify-between max-w-7xl mx-auto sm:mb-10">
+				<div className="text-left max-w-2x">
+					<h1 className="text-5xl mb-6">{t('title')}</h1>
+					<p className="text-primary-200">{t('description')}</p>
+				</div>
+				<Link href="/gallery" className="group uppercase font-extralight py-8">
+					<div className="border-b-2 border-primary-400/30 text-primary-500 group-hover:text-primary-400 group-hover:border-primary-400/50 transition-colors duration-300">
+						{t('fullLink')}
+					</div>
+				</Link>
+			</div>
+			<div className="max-w-7xl mx-auto grid sm:grid-cols-2 lg:grid-cols-3 sm:gap-x-4">
+				<div className="grid gap-y-4">
+					<Image
+						src="/assets/photos/scenery/monastery/IMG_1575.webp"
+						width={1536 / 2}
+						height={2048 / 2}
+						alt="Indonesian Orthodox Monastery"
+						className="rounded-sm"
+					/>
+				</div>
+				<div className="grid gap-y-4">
+					<Image
+						src="/assets/photos/communities/jspp/IMG_1701.JPEG"
+						width={2048 / 2}
+						height={1536 / 2}
+						alt="Orthodox Church of Indonesia celebrating feast on Parish St. Peter & Paul"
+						className="rounded-sm h-full object-cover"
+					/>
+					<Image
+						src="/assets/photos/wide/homilies/bishop-daniel-with-laities.jpeg"
+						width={2522 / 2}
+						height={1248 / 2}
+						alt="Bishop Daniel giving homilies in St. Basil the Great"
+						className="rounded-sm h-full object-cover"
+					/>
+				</div>
+				<div className="grid gap-y-4">
+					<div className="rounded-sm bg-primary h-full w-full"></div>
+				</div>
+			</div>
+		</div>
+	)
+}
