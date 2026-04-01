66aa1c5b5464ce8e90e3a1eab515d7ebc4ce64ee
Fray117 <fray117@exploiter.id>
2026-04-02 01:35:26 +0700

    feat(theme-blocks/blog/index.ts): add exports for Headline, Hero, Featured, FeaturedImage, FeedCard, RelatedFeed, and CTA components

---
+++ src/components/theme-blocks/blog/index.ts
@@ -0,0 +1,8 @@
+export { default as Headline } from "./Headline"
+export { default as Hero } from "./Hero"
+export { default as Featured } from "./Featured"
+export { default as FeaturedImage } from "./FeaturedImage"
+export { default as FeedCard } from "./FeedCard"
+export { default as RelatedFeed } from "./RelatedFeed"
+export { default as RelatedTags } from "./RelatedTags"
+export { default as CTA } from "./CTA"
