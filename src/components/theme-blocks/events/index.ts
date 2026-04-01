6d9b3ed0c6007e88950e524fd056c4ea8d65900e
Fray117 <fray117@exploiter.id>
2026-04-02 01:39:33 +0700

    feat(theme-blocks/events): add index.ts file to export theme blocks events components

---
+++ src/components/theme-blocks/events/index.ts
@@ -0,0 +1,4 @@
+export { default as Hero } from "./Hero"
+export { default as FilterBar } from "./FilterBar"
+export { default as Sidebar } from "./Sidebar"
+export { default as Timeline } from "./Timeline"
