9641e2659dfc7d5cde07e506eebde2cdcb7c0be1
Fray117 <fray117@exploiter.id>
2026-04-02 01:28:56 +0700

    feat(page.tsx): add Home page with Navbar, Hero, About, Virtue, Apostolic, Gallery and CTA components using Next.js internationalization features

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/components/theme-blocks/global"
 import { About, Apostolic, CTA, Gallery, Hero, Virtue } from "@/components/theme-blocks/home"
 import { getTranslations } from "next-intl/server"
 
@@ -6,19 +7,22 @@ export async function generateMetadata({ params }: { params: Promise<{ locale: s
 	const t = await getTranslations({ locale, namespace: 'Metadata' })
 
 	return {
-		title: t('title')
+		title: t('home.title')
 	}
 }
 
 export default function Home() {
 	return (
-		<main className="flex flex-col flex-1 items-center justify-center font-sans ">
-			<Hero />
-			<About />
-			<Virtue />
-			<Apostolic />
-			<Gallery />
-			<CTA />
-		</main>
+		<>
+			<Navbar hero />
+			<main className="flex flex-col flex-1 items-center justify-center font-sans ">
+				<Hero />
+				<About />
+				<Virtue />
+				<Apostolic />
+				<Gallery />
+				<CTA />
+			</main>
+		</>
 	)
 }
