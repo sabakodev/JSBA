5bda114405ba0fa641e8d3e16f22aabe18121bec
Fray117 <fray117@exploiter.id>
2026-04-03 01:58:08 +0700

    fix(layout.tsx): wrap children with TooltipProvider to provide tooltip context feat(layout.tsx): add Navbar component and render it within the layout

--- src/app/[locale]/(pages)/layout.tsx
+++ src/app/[locale]/(pages)/layout.tsx
@@ -1,12 +1,14 @@
 import { Navbar } from "@/components/theme-blocks/global"
+import { TooltipProvider } from "@/components/ui/tooltip"
 
 export default function LayoutComponent({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<>
+		<TooltipProvider>
 			<Navbar />
 			{children}
-		</>
+		</TooltipProvider>
+
 	)
 }
