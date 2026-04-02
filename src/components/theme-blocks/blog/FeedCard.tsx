7c73c4f37b29105d47b46a7ab4e62b713eec691d
Fray117 <fray117@exploiter.id>
2026-04-02 19:54:28 +0700

    fix(FeedCard.tsx): update props and usage to use GraphQL Post type instead of individual properties

--- src/components/theme-blocks/blog/FeedCard.tsx
+++ src/components/theme-blocks/blog/FeedCard.tsx
@@ -1,43 +1,39 @@
 import { Link } from "@/i18n/nav"
+import { Post } from "@/lib/graphql/queries/posts"
 import { useTranslations } from "next-intl"
 import Image from "next/image"
 
 interface Props {
-	href: string
-	title: string
-	category: string
-	excerpt: string
-	image: string
-	imageAlt: string
-	publishedAt: Date
+	data: Post
 }
 
-export default function Component({ href, title, category, excerpt, image, imageAlt, publishedAt }: Props) {
+export default function Component({ data }: Props) {
 	const t = useTranslations('blogPage.feed')
 
+	const altText = data.featuredImage ? data.featuredImage.node.altText : 'Featured image unavailable'
+	const sourceUrl = data.featuredImage ? data.featuredImage.node.sourceUrl : '/assets/photos/scenery/monastery/IMG_1575.webp'
+
 	return (
-		<Link href={href}>
+		<Link href={`/blog/${data.slug}`}>
 			<article className="flex flex-col space-y-6 group">
-				<div className="aspect-4/5 overflow-hidden rounded-lg bg-surface-container">
+				<div className="aspect-5/4 overflow-hidden rounded-lg bg-surface-container">
 					<Image
 						className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105 sepia-[0.2]"
-						alt={imageAlt}
+						alt={altText}
 						width={512}
 						height={512}
-						src={image} />
+						src={sourceUrl} />
 				</div>
 				<div className="space-y-3">
 					<div className="flex justify-between items-center">
-						<span className="text-[10px] uppercase tracking-[0.2em] text-primary">{category}</span>
-						<span className="text-[10px] uppercase tracking-widest text-outline">{publishedAt.toDateString()}</span>
+						<div className="text-[10px] uppercase tracking-[0.2em] text-primary">{data.categories.nodes[0].name}</div>
+						<span className="text-[10px] uppercase tracking-widest text-outline">{new Date(data.date).toDateString()}</span>
 					</div>
 					<h3
-						className="text-2xl font-serif text-secondary leading-tight group-hover:text-primary transition-colors">
-						{title}
+						className="text-2xl font-serif text-secondary leading-tight group-hover:text-primary transition-colors line-clamp-1">
+						{data.title}
 					</h3>
-					<p className="text-sm text-secondary font-body leading-relaxed line-clamp-3">
-						{excerpt}
-					</p>
+					<p className="text-sm text-secondary font-body leading-relaxed line-clamp-3" dangerouslySetInnerHTML={{ __html: data.excerpt }} />
 					<button
 						className="pt-2 text-xs uppercase tracking-widest text-primary border-b border-transparent group-hover:border-primary transition-all">
 						{t('readMore')}
