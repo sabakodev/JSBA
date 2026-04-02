cb6c2d71e10c627fcab0821e42f3f2b2b0c34e51
Fray117 <fray117@exploiter.id>
2026-04-03 01:58:29 +0700

    fix(config): add allowedDevOrigins and images.remotePatterns to next.config.ts for improved development experience feat(config): enable remote image patterns in next.config.ts for better handling of external images

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,23 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
 	reactCompiler: true,
-
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'jsba.sabako.id',
+			},
+			{
+				protocol: 'https',
+				hostname: 'secure.gravatar.com',
+			},
+		]
+	},
+	allowedDevOrigins: ['192.168.1.12'],
 }
 
 const withNextIntl = createNextIntlPlugin()
