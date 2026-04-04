268e9d3b5d5d2c96c1f9e7628bdc6cd18ed8e61e
Fray117 <fray117@exploiter.id>
2026-04-04 17:13:08 +0700

    fix(layout.tsx): update layout component to use correct class names and add responsive padding

--- src/app/[locale]/(pages)/(legal)/layout.tsx
+++ src/app/[locale]/(pages)/(legal)/layout.tsx
@@ -2,7 +2,7 @@ export default function LayoutComponent({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<div className="max-w-2xl mx-auto mt-17 pb-32 pt-16">
+		<div className="max-w-2xl mx-auto mt-17 mb-32 sm:pt-16 px-6">
 			{children}
 		</div>
 	)
