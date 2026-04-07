a44b8207e16d65f8c4b800a130ed3a8b40e9f251
Fray117 <fray117@exploiter.id>
2026-04-08 02:43:56 +0700

    fix(FilterBar.tsx): update FilterBar component to use categories and category props, add support for handling category changes

--- src/components/theme-blocks/events/FilterBar.tsx
+++ src/components/theme-blocks/events/FilterBar.tsx
@@ -1,43 +1,66 @@
+'use client'
+
+import { usePathname, useRouter } from "@/i18n/nav"
+import { EventType } from "@/lib/graphql/queries/events"
 import { cn } from "@/lib/utils"
 import { ChevronDown } from "lucide-react"
 import { useTranslations } from "next-intl"
 
-export default function Component() {
+export default function Component({
+	categories,
+	category
+
+}: {
+	categories: EventType[]
+	category?: string
+}) {
 	const t = useTranslations('eventPage.filter')
 
-	const filters = [
-		{
-			label: 'All Events',
-			current: true,
-		},
-		{
-			label: 'Liturgical',
-			current: false,
-		},
-		{
-			label: 'Educational',
-			current: false,
-		},
-		{
-			label: 'Community',
-			current: false,
-		},
-	]
+	const router = useRouter()
+	const pathname = usePathname()
+
+	const handleCategoryClick = (slug?: string) => {
+		const params = new URLSearchParams({ category: category ?? '' })
+
+		// Reset to page 1 when changing category
+		params.delete("page")
+		params.delete("cursor")
+
+		if (slug) {
+			params.set("category", slug)
+		} else {
+			params.delete("category")
+		}
+
+		const query = params.toString()
+		router.push(query ? `${pathname}?${query}` : pathname)
+	}
 
 	return (
 		<div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-20 gap-8">
 			<div className="flex flex-wrap gap-4">
+				<button
+					onClick={() => handleCategoryClick()}
+					className={cn(
+						"text-xs uppercase tracking-widest px-5 py-2 rounded-lg border",
+						category ? "bg-secondary-950/10 text-secondary border-border/30 hover:border-primary/50 transition-colors" :
+							"bg-secondary-900 text-primary border-primary/10"
+					)}
+				>
+					All
+				</button>
 				{
-					filters.map(({ label, current }, index) => (
+					categories.map(({ name, slug }, index) => (
 						<button
 							key={index}
+							onClick={() => handleCategoryClick(slug)}
 							className={cn(
 								"text-xs uppercase tracking-widest px-5 py-2 rounded-lg border",
-								current ? "bg-secondary-900 text-primary border-primary/10" :
+								category === slug ? "bg-secondary-900 text-primary border-primary/10" :
 									"bg-secondary-950/10 text-secondary border-border/30 hover:border-primary/50 transition-colors"
 							)}
 						>
-							{label}
+							{name}
 						</button>
 					))
 				}
