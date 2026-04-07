94b480573b06bf1aed4037e0bdba711dfca2936c
Fray117 <fray117@exploiter.id>
2026-04-08 02:44:13 +0700

    fix(page.tsx): add support for search parameters and display filtered events feat(page.tsx): fetch categories and events data using GraphQL services and display them in the page

--- src/app/[locale]/(pages)/event/page.tsx
+++ src/app/[locale]/(pages)/event/page.tsx
@@ -1,4 +1,5 @@
 import { FilterBar, Hero, Sidebar, Timeline } from "@/components/theme-blocks/events"
+import { getCategories, getEvents } from "@/lib/graphql/services/events"
 import { getTranslations } from "next-intl/server"
 
 export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
@@ -12,18 +13,43 @@ export async function generateMetadata({ params }: { params: Promise<{ locale: s
 
 export default async function Page({
 	params,
+	searchParams,
 }: Readonly<{
-	params: Promise<{ locale: string }>
+	params: Promise<{
+		locale: string
+	}>
+	searchParams: Promise<{
+		page?: string
+		category?: string
+	}>
 }>) {
 	const { locale } = await params
+
+	const query = await searchParams
+	const activeCategory = query.category || undefined
+
+	const now = new Date()
+	const yesterday = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1)
+
+	const [categories, eventsData] = await Promise.all([
+		getCategories(),
+		getEvents({
+			first: 20,
+			afterDate: yesterday,
+			categorySlug: activeCategory,
+		})
+	])
 
 	return (
 		<>
 			<Hero />
 			<main className="max-w-7xl mx-auto px-6 py-24">
-				<FilterBar />
+				<FilterBar
+					categories={categories}
+					category={activeCategory}
+				/>
 				<div className="grid grid-cols-1 lg:grid-cols-12 gap-24">
-					<Timeline />
+					<Timeline events={eventsData} />
 					<Sidebar locale={locale} />
 				</div>
 			</main>
